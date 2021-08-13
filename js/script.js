32b4d5302964b9fe2ea60fc3883bc44fe3fda733
Anton <mgyp@mail.ru>
2021-08-13 15:57:10 +0300

    new

---
+++ js/script.js
@@ -0,0 +1,74 @@
+
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string'*9);
+
+
+// const person = 'Alex Adipisicing excepteur aliqua aute amet magna tempor anim';
+
+// const bool = false;
+
+// let und;
+
+// console.log(und);
+
+// const obj = {
+//     name: 'Jhon',
+//     age: 25,
+//     isMaried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj.age);
+
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr);
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Ваше имя?', '');
+// answers[1] = prompt('Ваше фамиляя?', '');
+// answers[2] = prompt('Ваше возраст?', '');
+
+// console.log(typeof(answers));
+
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`Привет, ${user}`);
+
+// console.log(4 + '5');
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log((2 + 2) * 2 !== '6');
+
+
+
+const isChecked = false,
+        isClose = false;
+
+        console.log(isChecked || !isClose);
---
+++ script.js
@@ -0,0 +1,1 @@
+asnkjdfbajshbdfkjshb
