f4062a7d180741956a4d2dfd2ca0ef0d888e0b2a
Anton <mgyp@mail.ru>
2021-08-17 15:41:31 +0300

    027 Задачи с собеседований на понимание основ

---
+++ js/27.js
@@ -0,0 +1,32 @@
+// let x = 5; alert( x++ );
+
+// [ ] + false - null + true; 
+
+// console.log(typeof([] + false)); //false
+// console.log([] + false - null + true); //NaN
+
+// let y = 1;
+// let x = y = 2;
+// alert(x); // 2
+
+// console.log([ ] + 1 + 2); //'12'
+
+// alert( "1"[0] ); // 1
+
+
+// console.log(2 && 1 && null && 0 && undefined); //null
+
+
+// console.log(!!( 1 && 2 ) === (1 && 2)); //fasle
+
+// alert( null || 2 && 3 || 4 ); // '3'
+    
+// let a = [1, 2, 3];
+// let b = [1, 2, 3]; 
+// console.log(a == b);
+
+// alert( +"Infinity" ); 
+
+// console.log("Ёжик" > "яблоко"); //false
+
+console.log(0 || "" || 2 || undefined || true || falsе);
